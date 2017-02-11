63cb3f1c9aa7192801b868b20ff68a521864d9e5
IvanASUS <bobmarvel@yandex.ru>
2017-02-12 00:42:17 +0300

    CHAPTER5 NAV

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,9 +34,10 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+  goHome() {
+    this.nav.push(MyTeamsPage);
+  }
+  goToTournaments() {
+    this.nav.push(TournamentsPage);
   }
 }
